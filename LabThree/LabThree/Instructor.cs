fe1c83d1cc0bd2a2466c5b3e9685f5ab464533dd
Neisha Fannin <Ms.Latriece@yahoo.com>
2016-03-12 17:11:27 -0500

    Lab Three

---
+++ LabThree/LabThree/Instructor.cs
@@ -0,0 +1,32 @@
+﻿namespace LabThree
+{
+    class Instructor
+    {
+
+        private string name;
+        private string courseName;
+
+        public Instructor (string name, string courseName)
+        {
+            this.name = name;
+            this.courseName = courseName; 
+        }
+
+        public void SetStudentGrade (Student student , int grade)
+        {
+            student.SetGrade(grade);
+        
+        }
+
+        public void Print()
+        {
+            
+            System.Console.Write("INSTRUCTOR Name: " + name + "   Course: " + courseName + "     ");
+
+            
+
+        }
+
+
+    }
+}
---
+++ LabThree/LabThree/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace LabThree
+{
+    class Student
+    {
+        private string name;
+        private int grade;
+        private Instructor teacher;
+
+
+        public Student (string name, Instructor teacher)
+        {
+            this.name = name;
+            this.teacher = teacher;
+            this.grade = 0;
+        }
+
+        public void SetGrade(int grade)
+        {
+            this.grade = grade;
+        }
+
+        public void Print()
+        {
+            teacher.Print();
+            System.Console.WriteLine("STUDENT  Name: " + name + "   Grade: " + grade);
+            
+           
+
+        }
+
+    }
+}
